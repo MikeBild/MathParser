62a03f871117fbb6f27e5a782d60afcee100a513
MikeBild <mike@mikebild.com>
2010-02-07 19:30:14 +0100

    Code CleanUp

---
+++ MathParserTests/ParenOperationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathParser;
+using NUnit.Framework;
+
+namespace MathParserTests
+{
+    [TestFixture]
+    public class ParenOperationTests
+    {
+        private Compiler _compiler = null;
+        private Func<double> _sut = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _compiler = new Compiler();
+        }
+
+        [Test]
+        public void ParserBasicParenTest()
+        {
+            _sut = _compiler.Compile("(1.11+1.2)");
+            Assert.That(_sut(), Is.EqualTo((1.11 + 1.2)));
+        }
+    }
+}
