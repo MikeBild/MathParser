31222ef245439383ecbf1b3f6b30498fe8f6bc68
MikeBild <mike@mikebild.com>
2010-02-08 03:17:50 +0100

    Code cleanup

--- ClassicMathParser/Lexer.cs
+++ ClassicMathParser/Lexer.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace ClassicMathParser
@@ -9,52 +7,107 @@ namespace ClassicMathParser
     public class Lexer
     {
         internal readonly Token[] _tokens;
-        private bool _goReverse = false;
-        private Token _previousToken = null;
-
-        private int _current = 0;
-
-        private static Token[] Tokenize(string equation)
-        {
-            var RE = new Regex(@"([\+\-\*\(\)\^\/])");
-            var tokens = (RE.Split(equation).Where(f => f != "").Select(f =>
-                                                  {
-                                                      if (f == "+")
-                                                          return new Token { TokenType = TokenType.Plus };
-                                                      if (f == "*")
-                                                          return new Token { TokenType = TokenType.Multiply };
-                                                      if (f == "-")
-                                                          return new Token { TokenType = TokenType.Minus };
-                                                      if (f == "/")
-                                                          return new Token { TokenType = TokenType.Divide };
-                                                      if (f == "x")
-                                                          return new Token { TokenType = TokenType.Parameter };
-                                                      if (f == "^")
-                                                          return new Token { TokenType = TokenType.Quadrat };
-                                                      if (f == "(")
-                                                          return new Token { TokenType = TokenType.LParen };
-                                                      if (f == ")")
-                                                          return new Token { TokenType = TokenType.RParen };
-                                                      if (f == "sin(x)")
-                                                          return new Token { TokenType = TokenType.Sin };
-
-                                                      return new Token { TokenType = TokenType.Number, Value = f.ToDouble() };
-                                                  })).ToList();
-            tokens.Add(new Token { TokenType = TokenType.End });
-            return tokens.ToArray();
-        }
+        private int _current;
+        private bool _goReverse;
+        private Token _previousToken;
 
         public Lexer(string input)
         {
             _tokens = Tokenize(input);
         }
 
+        public Token Current
+        {
+            get { return _tokens[_current]; }
+        }
+
+        private static Token[] Tokenize(string equation)
+        {
+            var RE = new Regex(@"([\+\-\*\(\)\^\/])");
+            List<Token> tokens = (RE.Split(equation).Where(f => f != "").Select(f =>
+                                                                                    {
+                                                                                        if (f == "+")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Plus
+                                                                                                       };
+                                                                                        if (f == "*")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Multiply
+                                                                                                       };
+                                                                                        if (f == "-")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Minus
+                                                                                                       };
+                                                                                        if (f == "/")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Divide
+                                                                                                       };
+                                                                                        if (f == "x")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .
+                                                                                                               Parameter
+                                                                                                       };
+                                                                                        if (f == "^")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Quadrat
+                                                                                                       };
+                                                                                        if (f == "(")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .LParen
+                                                                                                       };
+                                                                                        if (f == ")")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .RParen
+                                                                                                       };
+                                                                                        if (f == "sin(x)")
+                                                                                            return new Token
+                                                                                                       {
+                                                                                                           TokenType =
+                                                                                                               TokenType
+                                                                                                               .Sin
+                                                                                                       };
+
+                                                                                        return new Token
+                                                                                                   {
+                                                                                                       TokenType =
+                                                                                                           TokenType.
+                                                                                                           Number,
+                                                                                                       Value =
+                                                                                                           f.ToDouble()
+                                                                                                   };
+                                                                                    })).ToList();
+            tokens.Add(new Token {TokenType = TokenType.End});
+            return tokens.ToArray();
+        }
+
         public void Reverse()
         {
             _current--;
         }
-
-        public Token Current { get { return _tokens[_current]; } }
 
         public Token MoveNext()
         {
